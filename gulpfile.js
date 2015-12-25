982afa3bc9fe30713e80b084b6ebe613f1907998
Sai <charankuppa@gmail.com>
2015-12-26 01:59:05 +0530

    Adds index.html

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('sass',function(){
 
 gulp.task('watch',['browserSync','sass'],function(){
 	gulp.watch('app/scss/**/*.+(scss|sass)', ['sass']);
-	gulp.watch('app/*.html',browserSync.reload)
+	gulp.watch('*.html',browserSync.reload)
 })
 
 gulp.task('browserSync',function(){
